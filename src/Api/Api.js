6c3c3771f3ddb8b24fc9ce71ffda06769681f6d0
Rushikeshsasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2024-01-11 23:47:42 +0530

    first commit

---
+++ src/Api/Api.js
@@ -0,0 +1,11 @@
+
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '42d976abb7msh7edd9128413046fp1836ecjsndfe77586e5f1',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities"
